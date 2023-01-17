b53237a2d5da0ef6cd11604ec161e3f04ec34730
Ulyana-AA <98107469+Ulyana-AA@users.noreply.github.com>
2023-01-17 11:25:42 +0300

    Update Policyholder.cs

--- dzporpm-1/Policyholder.cs
+++ dzporpm-1/Policyholder.cs
@@ -3,5 +3,6 @@
     internal class Policyholder : Client
     {
         List<InsuranceContract> insuranceContracts = new List<InsuranceContract>();
+        List<Beneficiary> beneficiarys = new List<Beneficiary>();
     }
 }
